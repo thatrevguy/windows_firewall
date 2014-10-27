72cfc78374bbe32b1d2af87bb048420710dde27c
Trevor Howard <thoward@godaddy.com>
2014-10-26 23:42:19 -0700

    Removed debug code from validate_rule.pp

--- templates/validate_rule.ps1
+++ templates/validate_rule.ps1
@@ -2,4 +2,4 @@
 <%= scope.function_template(["windows_firewall/rule_object.ps1"]) %>
 
 #Check if rule property values match
-foreach($PropertyName in ($Rule | Get-Member).Name){foreach($FoundRule in $FoundRules){if($FoundRule.$PropertyName -ne $Rule.$PropertyName){ $FoundRule | Out-File -Path ($FoundRule.Name + "_System.txt"); $Rule | Out-File -Path ($Rule.Name + "_Puppet.txt"); exit 1 }}}
+foreach($PropertyName in ($Rule | Get-Member).Name){foreach($FoundRule in $FoundRules){if($FoundRule.$PropertyName -ne $Rule.$PropertyName){ exit 1 }}}
