30d18bcda0c4fb34f580909ba587675ca672f6ee
Trevor Howard <thoward@godaddy.com>
2014-10-28 13:06:50 -0700

    Added windows_firewall_rule_report custom fact

--- facts.d/report_rule.ps1
+++ facts.d/report_rule.ps1
@@ -1,1 +1,3 @@
-#Returns key value pair of all enabled firewall rule properties in json string. Auditing will need to develop a process on how to interpret data. WIll be stored in PuppetDB.
+#Returns key value pair of all enabled firewall rule properties in json string. Auditing will need to develop a process on how to interpret data. Will be stored in PuppetDB.
+$Rules = $WindowsFirewall.Rules | where {$_.Direction -eq 1 -and $_.Enabled -eq 'True'} | Select-Object Name,LocalPorts,RemoteAddresses,Action | ConvertTo-Json -Compress
+Write-Host "windows_firewall_rule_report=$Rules"
