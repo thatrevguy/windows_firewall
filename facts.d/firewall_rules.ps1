7ee792a3c8630442e46b9047c33ac6d550765473
Trevor Howard <thoward@godaddy.com>
2014-10-29 10:06:47 -0700

    Added protocol property to windows_firewall fact

--- facts.d/firewall_rules.ps1
+++ facts.d/firewall_rules.ps1
@@ -1,1 +1,1 @@
-Write-Host "firewall_rules="((New-Object -ComObject HNetCfg.FWPolicy2).Rules | where {$_.Direction -eq 1 -and $_.Enabled -eq 'True'} | Select-Object Name,LocalPorts,RemoteAddresses,Action | ConvertTo-Json -Compress)
+Write-Host "firewall_rules="((New-Object -ComObject HNetCfg.FWPolicy2).Rules | where {$_.Direction -eq 1 -and $_.Enabled -eq 'True'} | Select-Object Name,Protocol,LocalPorts,RemoteAddresses,Action | ConvertTo-Json -Compress)
