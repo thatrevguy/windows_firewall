7a208eddbb386730d0a48e9034f3bede698d1c20
Trevor Howard <thoward@godaddy.com>
2014-12-05 12:50:53 -0700

    Fixed syntax issue

--- manifests/rule_controller.pp
+++ manifests/rule_controller.pp
@@ -3,7 +3,7 @@ class windows_firewall::rule_controller (
 	$rule_key,
 ){
     if $enabled == true {
-	    $rules = hiera_hash('${rule_key}')
+	    $rules = hiera_hash($rule_key)
         exec { 'apply_rules':
             command => template('windows_firewall/apply_rules.ps1'),
             unless => template('windows_firewall/validate_rules.ps1'),
