aa673a79804e23538ba72993a4a2ca46e75f1b7a
Trevor Howard <thoward@godaddy.com>
2014-12-11 12:00:52 -0700

    Testing custom type 'firewall_rule'

--- lib/puppet/provider/firewall_rule/rule.rb
+++ lib/puppet/provider/firewall_rule/rule.rb
@@ -1,14 +1,36 @@
+require 'win32ole'
 Puppet::Type.type(:firewall_rule).provide(:rule) do
   desc "Operate on rules for ensurable"
 
+  system_rules = WIN32OLE.new("HNetCfg.FwPolicy2").rules
+  system_rules.each do |system_rule|
+    puts rule.name
+  end
+  
   def rule_hash
+    rule_attr = Hash.new()
+    rule_attr['ensure'] = [nil, 'present']
+    rule_attr['description'] = ['Description', '']
+    rule_attr['application_name'] = ['ApplicationName', '']
+    rule_attr['service_name'] = ['serviceName', '']
+    rule_attr['protocol'] = ['Protocol', 'TCP']
+    rule_attr['local_ports'] = ['LocalPorts', '']
+    rule_attr['remote_ports'] = ['RemotePorts', '']
+    rule_attr['local_addresses'] = ['LocalAddresses', '']
+    rule_attr['remote_addresses'] = ['RemoteAddresses', '']
+    rule_attr['icmp_types_and_codes'] = ['IcmpTypesAndCodes', '']
+    rule_attr['direction'] = ['Direction', 'In']
+    rule_attr['interfaces'] = ['Interfaces', nil]
+    rule_attr['interface_types'] = ['InterfaceTypes', 'All']
+    rule_attr['enabled'] = ['Enabled', 'True']
+    rule_attr['grouping'] = ['Grouping', '']
+    rule_attr['profiles'] = ['Profiles', 'Domain,Private,Public']
+    rule_attr['edge_traversal'] = ['EdgeTraversal', 'False']
+    rule_attr['action'] = ['Action', 'Allow']
+    rule_attr['edge_traversal_options'] = ['EgdeTraversalOptions', 'Block']
+	
     should_rule = @resource.should(:rule_hash)
 	puts should_rule
-    #require 'win32ole'
-    #firewall = WIN32OLE.new("HNetCfg.FwPolicy2")
-    #firewall.rules.each do |rule|
-    #  puts rule.name
-    #end
   end
   
   def rule_hash=(value)
--- lib/puppet/type/firewall_rule.rb
+++ lib/puppet/type/firewall_rule.rb
@@ -10,33 +10,22 @@ Puppet::Type.newtype(:firewall_rule) do
   newproperty(:rule_hash) do
     desc "Apply firewall rule hash from Hiera."
 
-    rule_attr_name = Hash.new()
-    rule_attr_name['description'] = [nil, 'Description', nil]
-    rule_attr_name['application_name'] = [nil, 'ApplicationName', nil]
-    rule_attr_name['service_name'] = [nil, 'serviceName', nil]
-    rule_attr_name['protocol'] = [/^(ICMPv4|IGMP|TCP|UDP|IPv6|IPv6Route|IPv6Frag|GRE|ICMPv6|IPv6NoNxt|IPv6Opts|VRRP|PGM|L2TP|1|2|6|17|41|43|44|47|58|59|60|112|113|115)$/i, 'Protocol', 'TCP']
-    rule_attr_name['local_ports'] = [nil, 'LocalPorts', nil]
-    rule_attr_name['remote_ports'] = [nil, 'RemotePorts', nil]
-    rule_attr_name['local_addresses'] = [nil, 'LocalAddresses', nil]
-    rule_attr_name['remote_addresses'] = [nil, 'RemoteAddresses', nil]
-    rule_attr_name['icmp_types_and_codes'] = [nil, 'IcmpTypesAndCodes', nil]
-    rule_attr_name['direction'] = [/^(In|Out|1|2)$/i, 'Direction', 'In']
-    rule_attr_name['interfaces'] = [nil, 'Interfaces', nil]
-    rule_attr_name['interface_types'] = [/^(((Wireless|Lan|RemoteAccess)(,(?!$))?(?!\3)){1,2}|All)$/i, 'InterfaceTypes', 'All']
-    rule_attr_name['enabled'] = [/^(True|False)$/i, 'Enabled', 'True']
-    rule_attr_name['grouping'] = [nil, 'Grouping', nil]
-    rule_attr_name['profiles'] = [/^(((Domain|Private|Public)(,(?!$))?(?!.*\3)){1,3}|1|2|3|4|5|6|7|2147483647)$/i, 'Profiles', 'Domain,Private,Public']
-    rule_attr_name['edge_traversal'] = [/^(True|False)$/i, 'EdgeTraversal', 'False']
-    rule_attr_name['action'] = [/^(Allow|Block|1|0)$/i, 'Action', 'Allow']
-    rule_attr_name['edge_traversal_options'] = [/^(Block|Allow|Defer to App|Defer to User|0|1|2|3)$/i, 'EgdeTraversalOptions', 'Block']
+    validation_hash = Hash.new()
+	validation_hash['ensure'] = /^(present|absent)$/i
+    validation_hash['protocol'] = /^(ICMPv4|IGMP|TCP|UDP|IPv6|IPv6Route|IPv6Frag|GRE|ICMPv6|IPv6NoNxt|IPv6Opts|VRRP|PGM|L2TP|1|2|6|17|41|43|44|47|58|59|60|112|113|115)$/i
+    validation_hash['direction'] = /^(In|Out|1|2)$/i
+    validation_hash['interface_types'] = /^(((Wireless|Lan|RemoteAccess)(,(?!$))?(?!\3)){1,2}|All)$/i
+    validation_hash['enabled'] = /^(True|False)$/i
+    validation_hash['profiles'] = /^(((Domain|Private|Public)(,(?!$))?(?!.*\3)){1,3}|1|2|3|4|5|6|7|2147483647)$/i
+    validation_hash['edge_traversal'] = /^(True|False)$/i
+    validation_hash['action'] = /^(Allow|Block|1|0)$/i
+    validation_hash['edge_traversal_options'] = /^(Block|Allow|Defer to App|Defer to User|0|1|2|3)$/i
 
     validate do |value|
       value.each do |name, rule|
-        rule_attr_name.keys.each do |key|
-          if rule_attr_name[key][0]
-            message = "Rule \'%s\' %s attribute value of \'%s\' is invalid." % [name, key, rule[key].to_s]
-            raise ArgumentError, message if rule[key].to_s !~ rule_attr_name[key][0]
-          end
+        validation_hash.keys.each do |key|
+          message = "Rule \'%s\' %s attribute value of \'%s\' is invalid." % [name, key, rule[key].to_s]
+          raise ArgumentError, message if rule[key].to_s !~ validation_hash[key]
         end
       end
     end
