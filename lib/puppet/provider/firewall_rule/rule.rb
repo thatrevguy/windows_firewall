1dad43f6d9a1a2899ba9db73f95c5ecda93d34d2
Trevor Howard <thoward@godaddy.com>
2014-12-11 10:46:58 -0700

    Testing custom type 'firewall_rule'

--- lib/puppet/provider/firewall_rule/rule.rb
+++ lib/puppet/provider/firewall_rule/rule.rb
@@ -1,15 +1,11 @@
 Puppet::Type.type(:firewall_rule).provide(:rule) do
   desc "Operate on rules for ensurable"
 
-  def apply_rule
+  def rule_hash
     require 'win32ole'
     firewall = WIN32OLE.new("HNetCfg.FwPolicy2")
     firewall.rules.each do |rule|
       puts rule.name
     end
-  end
-  
-  def validate_rule
-  
   end
 end
