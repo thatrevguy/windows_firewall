de7c66665fe6120fee0bd2e4f086e1a63098b8ab
Trevor Howard <thoward@godaddy.com>
2014-12-11 12:26:29 -0700

    Testing custom type 'firewall_rule'

--- lib/puppet/provider/firewall_rule/rule.rb
+++ lib/puppet/provider/firewall_rule/rule.rb
@@ -1,36 +1,33 @@
 require 'win32ole'
 Puppet::Type.type(:firewall_rule).provide(:rule) do
-  desc "Operate on rules for ensurable"
+  desc "Configures rules"
 
   system_rules = WIN32OLE.new("HNetCfg.FwPolicy2").rules
-  system_rules.each do |system_rule|
-    puts system_rule.name
-  end
-  
+  should_rule = @resource.should(:rule_hash)
+
+  attr_hash = Hash.new()
+  attr_hash['ensure'] = [nil, 'present']
+  attr_hash['description'] = ['Description', '']
+  attr_hash['application_name'] = ['ApplicationName', '']
+  attr_hash['service_name'] = ['serviceName', '']
+  attr_hash['protocol'] = ['Protocol', 'TCP']
+  attr_hash['local_ports'] = ['LocalPorts', '']
+  attr_hash['remote_ports'] = ['RemotePorts', '']
+  attr_hash['local_addresses'] = ['LocalAddresses', '']
+  attr_hash['remote_addresses'] = ['RemoteAddresses', '']
+  attr_hash['icmp_types_and_codes'] = ['IcmpTypesAndCodes', '']
+  attr_hash['direction'] = ['Direction', 'In']
+  attr_hash['interfaces'] = ['Interfaces', nil]
+  attr_hash['interface_types'] = ['InterfaceTypes', 'All']
+  attr_hash['enabled'] = ['Enabled', 'True']
+  attr_hash['grouping'] = ['Grouping', '']
+  attr_hash['profiles'] = ['Profiles', 'Domain,Private,Public']
+  attr_hash['edge_traversal'] = ['EdgeTraversal', 'False']
+  attr_hash['action'] = ['Action', 'Allow']
+  attr_hash['edge_traversal_options'] = ['EgdeTraversalOptions', 'Block']
+
   def rule_hash
-    rule_attr = Hash.new()
-    rule_attr['ensure'] = [nil, 'present']
-    rule_attr['description'] = ['Description', '']
-    rule_attr['application_name'] = ['ApplicationName', '']
-    rule_attr['service_name'] = ['serviceName', '']
-    rule_attr['protocol'] = ['Protocol', 'TCP']
-    rule_attr['local_ports'] = ['LocalPorts', '']
-    rule_attr['remote_ports'] = ['RemotePorts', '']
-    rule_attr['local_addresses'] = ['LocalAddresses', '']
-    rule_attr['remote_addresses'] = ['RemoteAddresses', '']
-    rule_attr['icmp_types_and_codes'] = ['IcmpTypesAndCodes', '']
-    rule_attr['direction'] = ['Direction', 'In']
-    rule_attr['interfaces'] = ['Interfaces', nil]
-    rule_attr['interface_types'] = ['InterfaceTypes', 'All']
-    rule_attr['enabled'] = ['Enabled', 'True']
-    rule_attr['grouping'] = ['Grouping', '']
-    rule_attr['profiles'] = ['Profiles', 'Domain,Private,Public']
-    rule_attr['edge_traversal'] = ['EdgeTraversal', 'False']
-    rule_attr['action'] = ['Action', 'Allow']
-    rule_attr['edge_traversal_options'] = ['EgdeTraversalOptions', 'Block']
-	
-    should_rule = @resource.should(:rule_hash)
-	puts should_rule
+    puts should_rule
   end
   
   def rule_hash=(value)
