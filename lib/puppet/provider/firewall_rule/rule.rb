1d1921a5bf5b45ddad52a9d2cd800523ea60e900
Trevor Howard <thoward@godaddy.com>
2014-12-16 09:43:22 -0700

    testing munge

--- lib/puppet/provider/firewall_rule/rule.rb
+++ lib/puppet/provider/firewall_rule/rule.rb
@@ -59,8 +59,8 @@ Puppet::Type.type(:firewall_rule).provide(:rule) do
   end
   
   def rule_hash
-    File.open("C:\system_rules.txt", 'w') {|f| f.write(system_rule_hash(nil)) }
-    File.open("C:\json_rules.txt", 'w') {|f| f.write(@resource.should(:rule_hash)) }
+    File.new("C:\system_rules.txt", 'w') {|f| f.write(system_rule_hash(nil)) }
+    File.new("C:\json_rules.txt", 'w') {|f| f.write(@resource.should(:rule_hash)) }
     @resource.should(:rule_hash)
   end
   
