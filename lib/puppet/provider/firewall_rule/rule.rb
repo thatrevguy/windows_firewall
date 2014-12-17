b07abc55108891d6c68a38f84f718b83d2e86d9c
Trevor Howard <thoward@godaddy.com>
2014-12-17 11:37:47 -0700

    updating firewall_rule provider

--- lib/puppet/provider/firewall_rule/rule.rb
+++ lib/puppet/provider/firewall_rule/rule.rb
@@ -11,34 +11,32 @@ class Firewall
   end
 end
 
-def system_rule_hash(rulename)
+def system_rule_hash
   rules = WIN32OLE.new("HNetCfg.FwPolicy2").rules
   rule_hash = Hash.new()
 
   rules.each do |rule|
     if rule.enabled == true and rule.direction == 1
-      if rule.name == rulename or rulename == nil
-        attr_hash = Hash.new()
-        attr_hash['ensure'] = 'present'
-        attr_hash['description'] = rule.description
-        attr_hash['application_name'] = rule.applicationname
-        attr_hash['service_name'] = rule.servicename
-        attr_hash['protocol'] = rule.protocol
-        attr_hash['local_ports'] = rule.localports
-        attr_hash['remote_ports'] = rule.remoteports
-        attr_hash['local_addresses'] = rule.localaddresses
-        attr_hash['remote_addresses'] = rule.remoteaddresses
-        attr_hash['icmp_types_and_codes'] = rule.icmptypesandcodes
-        attr_hash['direction'] = rule.direction
-        attr_hash['interfaces'] = rule.interfaces
-        attr_hash['interface_types'] = rule.interfacetypes
-        attr_hash['enabled'] = rule.enabled
-        attr_hash['grouping'] = rule.grouping
-        attr_hash['profiles'] = rule.profiles
-        attr_hash['edge_traversal'] = rule.edgetraversal
-        attr_hash['action'] = rule.action
-        attr_hash['edge_traversal_options'] = rule.edgetraversaloptions
-      end
+      attr_hash = Hash.new()
+      attr_hash['ensure'] = 'present'
+      attr_hash['description'] = rule.description
+      attr_hash['application_name'] = rule.applicationname
+      attr_hash['service_name'] = rule.servicename
+      attr_hash['protocol'] = rule.protocol
+      attr_hash['local_ports'] = rule.localports
+      attr_hash['remote_ports'] = rule.remoteports
+      attr_hash['local_addresses'] = rule.localaddresses
+      attr_hash['remote_addresses'] = rule.remoteaddresses
+      attr_hash['icmp_types_and_codes'] = rule.icmptypesandcodes
+      attr_hash['direction'] = rule.direction
+      attr_hash['interfaces'] = rule.interfaces
+      attr_hash['interface_types'] = rule.interfacetypes
+      attr_hash['enabled'] = rule.enabled
+      attr_hash['grouping'] = rule.grouping
+      attr_hash['profiles'] = rule.profiles
+      attr_hash['edge_traversal'] = rule.edgetraversal
+      attr_hash['action'] = rule.action
+      attr_hash['edge_traversal_options'] = rule.edgetraversaloptions
 
       rule_hash[rule.name] = attr_hash
     end
@@ -63,7 +61,7 @@ Puppet::Type.type(:firewall_rule).provide(:rule) do
     #File.open(File.join('C:\\', 'system_rules.txt'), 'w') {|f| f.write(system_rule_hash(nil)) }
     #File.open(File.join('C:\\', 'json_rules.txt'), 'w') {|f| f.write(@resource.should(:rule_hash)) }
     #@resource.should(:rule_hash)
-	system_rule_hash(nil)
+	system_rule_hash
   end
   
   def rule_hash=(value)
