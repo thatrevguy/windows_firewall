0c5b12275a18694bdb392c6980aade144e0695c7
Trevor Howard <thoward@godaddy.com>
2014-12-17 13:59:02 -0700

    updating firewall_rule provider

--- lib/puppet/provider/firewall_rule/rule.rb
+++ lib/puppet/provider/firewall_rule/rule.rb
@@ -46,7 +46,7 @@ Puppet::Type.type(:firewall_rule).provide(:rule) do
   desc "Configures rules"
 
   def apply
-    if @resource.should(:apply) == 'false'
+    if !@resource.should(:apply)
       'false'
     else
       :true
