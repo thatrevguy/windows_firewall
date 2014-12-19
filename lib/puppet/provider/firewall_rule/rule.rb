0fb1934d6f68e9851f3029e79e57a01c9bb6d575
Trevor Howard <thoward@godaddy.com>
2014-12-19 14:45:14 -0700

    update to type and provider

--- lib/puppet/provider/firewall_rule/rule.rb
+++ lib/puppet/provider/firewall_rule/rule.rb
@@ -1,58 +1,150 @@
 require 'win32ole'
-require 'digest/md5'
 
-class Hash
-  def rkey_count(search)
-    i = 1
-    search = Regexp.new(search.to_s) unless search.is_a?(Regexp)
-    keys.each do |key|
-      if key =~ search
-       i += 1
+class WIN32OLE
+  def select(attr_name, value)
+    selected = Array.new()
+    self.each do |x|
+      x_value = x.invoke(attr_name)
+      if x_value =~ /^#{value}$/i or x_value == value
+        selected.push(x)
       end
     end
-    return i
+
+    return selected
   end
 end
 
-def attr_hash(rule)
-  attr_hash = Hash.new()
-  attr_hash['ensure'] = 'present'
-  attr_hash['description'] = rule.description
-  attr_hash['application_name'] = rule.applicationname
-  attr_hash['service_name'] = rule.servicename
-  attr_hash['protocol'] = rule.protocol
-  attr_hash['local_ports'] = rule.localports
-  attr_hash['remote_ports'] = rule.remoteports
-  attr_hash['local_addresses'] = rule.localaddresses
-  attr_hash['remote_addresses'] = rule.remoteaddresses
-  attr_hash['icmp_types_and_codes'] = rule.icmptypesandcodes
-  attr_hash['direction'] = rule.direction
-  attr_hash['interfaces'] = rule.interfaces
-  attr_hash['interface_types'] = rule.interfacetypes
-  attr_hash['enabled'] = rule.enabled
-  attr_hash['grouping'] = rule.grouping
-  attr_hash['profiles'] = rule.profiles
-  attr_hash['edge_traversal'] = rule.edgetraversal
-  attr_hash['action'] = rule.action
-  attr_hash['edge_traversal_options'] = rule.edgetraversaloptions
-  
-  return attr_hash
+def generate_rules(rule_hash)
+  rule_array = Array.new()
+  rule_hash.each do |name, rule|
+    if rule['ensure'] == 'present'
+      rule_attr = Hash.new()
+      rule_attr['name'] = name
+      rule.keys.each do |key|
+        if key != 'ensure'
+          rule_attr[key.split('_').join] = rule[key]
+        end
+      end
+
+      hnet_rule = WIN32OLE.new("HNetCfg.FWRule")
+      rule_attr.keys.each do |key|
+        if rule_attr[key]
+          message = "Rule \'%s\' %s attribute value of \'%s\' is invalid." % [name, key, rule_attr[key].to_s]
+          hnet_rule.setproperty(key, rule_attr[key]) rescue raise ArgumentError, message
+        end
+      end
+
+      rule_array.push(hnet_rule)
+    end
+  end
+
+  return rule_array
 end
 
-def system_rule_md5
-  rules = WIN32OLE.new("HNetCfg.FwPolicy2").rules
-  rule_hash = Hash.new()
+def validate_rule(system_rules, puppet_rule, attr_names)
+    system_rules.select('name', puppet_rule.name).each do |rule|
+      attr_names.each do |attr_name|
+        if rule.invoke(attr_name.to_s) != puppet_rule.invoke(attr_name.to_s)
+          return false
+        end
+      end
+    end
 
-  rules.each do |rule|
-    if rule.enabled == true
-      if rule_hash.has_key?(rule.name)
-        rule_hash["#{rule.name} #{rule_hash.rkey_count(/^#{rule.name}.*$/i)}"] = attr_hash(rule)
+  return true
+end
+
+def set_rule(system_rules, puppet_rule, attr_names)
+  def set_attr(sys_rule, puppet_rule, attr_names) 
+    attr_names.each do |attr_name|
+      if sys_rule.invoke(attr_name.to_s) != puppet_rule.invoke(attr_name.to_s)
+        sys_rule.setproperty(attr_name.to_s, puppet_rule.invoke(attr_name.to_s))
+      end
+    end
+  end
+
+  def attr_recovery(system_rule, error)
+    case error.to_s
+    when /.*OLE method `protocol':.*/i
+      system_rule.setproperty('IcmpTypesAndCodes',  nil)
+    else
+      raise WIN32OLERuntimeError, error
+    end
+  end
+
+  system_rules.select('name', puppet_rule.name).each do |rule|
+    begin
+      set_attr(rule, puppet_rule, attr_names)
+    rescue WIN32OLERuntimeError => error
+      attr_recovery(rule, error)
+      set_attr(rule, puppet_rule, attr_names)
+    end
+  end
+end
+
+def prune_rule(system_rules, name, count)
+  while count > 1 do
+    system_rules.remove(name)
+    count = count - 1
+  end
+end
+
+def ensure_rules(rule_hash, check_flag)
+  system_rules = WIN32OLE.new("HNetCfg.FwPolicy2").rules
+  puppet_rules = generate_rules(rule_hash)
+
+  def present_rules(system_rules, puppet_rules, check_flag)
+    attr_names = WIN32OLE.new("HNetCfg.FWRule").ole_get_methods
+    puppet_rules.each do |puppet_rule|
+      rule_count = system_rules.select('name', puppet_rule.name).count
+      if rule_count > 0
+        if !validate_rule(system_rules, puppet_rule, attr_names)
+          return false if check_flag
+          set_rule(system_rules, puppet_rule, attr_names)
+        end
       else
-        rule_hash[rule.name] = attr_hash(rule)
+        return false if check_flag
+        system_rules.add(puppet_rule)
+      end
+
+      if rule_count > 1
+        return false if check_flag
+        prune_rule(system_rules, puppet_rule.name, rule_count)
       end
     end
   end
-  Digest::MD5.hexdigest Marshal.dump(rule_hash.sort.to_s)
+
+  def absent_rules(system_rules, rule_hash, check_flag)
+    rule_hash.each do |name, rule|
+      if rule['ensure'] == 'absent'
+        rule_count = system_rules.select('name', name).count
+        while rule_count > 0 do
+          return false if check_flag
+          system_rules.remove(name)
+          rule_count = rule_count - 1
+        end
+      end
+    end
+  end
+
+  def disable_rules(system_rules, puppet_rules, check_flag)
+    system_rule_array = Array.new()
+    hash_rule_array = Array.new()
+    system_rules.each do |rule| system_rule_array.push(rule.name) end
+    puppet_rules.each do |rule| hash_rule_array.push(rule.name) end
+
+    (system_rule_array - hash_rule_array).sort.uniq.each do |name|
+      system_rules.select('name', name).each do |rule|
+        if rule.enabled
+          return false if check_flag
+          rule.setproperty('enabled', false)
+        end
+      end
+    end
+  end
+
+  return false if !present_rules(system_rules, puppet_rules, check_flag)
+  return false if !absent_rules(system_rules, rule_hash, check_flag)
+  return false if !disable_rules(system_rules, puppet_rules, check_flag)
 end
 
 Puppet::Type.type(:firewall_rule).provide(:rule) do
@@ -62,10 +154,14 @@ Puppet::Type.type(:firewall_rule).provide(:rule) do
     #File.open(File.join('C:\\', 'system_rules.txt'), 'w') {|f| f.write(system_rule_hash(nil)) }
     #File.open(File.join('C:\\', 'json_rules.txt'), 'w') {|f| f.write(@resource.should(:rule_hash)) }
     #@resource.should(:rule_hash)
-	system_rule_md5
+    if !ensure_rules(@resource.should(:rule_hash), true)
+      return "mismatch found"
+    else
+      return @resource.should(:rule_hash)
+    end
   end
   
   def rule_hash=(value)
-    puts 'change!'
+    ensure_rules(@resource.should(:rule_hash), false)
   end
 end
--- lib/puppet/type/firewall_rule.rb
+++ lib/puppet/type/firewall_rule.rb
@@ -33,87 +33,6 @@ Puppet::Type.newtype(:firewall_rule) do
         end
       end
     end
-
-    #Below methods and munging designed to replicate values that would be returned by HNetCfg.FWRule objects in a hash.
-	#Checksum of hash is sent to provider then compared.
-
-    #HNetCfg.FWRule sorts and formats RemoteAddresses and LocalAddresses attribute values.
-    #This method attempts to replicate formatting HNetCfg.FWRule performs. 
-    #Addresses are sorted unless an address is a range. Ranges are appended in order of entry.
-    #Addresses using CIDR are converted to use equivalent netmask value.
-    class String
-      def is_ip?
-        !!IPAddr.new(self) rescue false
-      end
-    end
-    
-    def netmask(address, version)
-      netmask = address.split('/')
-      if netmask[1]
-        return IPAddr.new('255.255.255.255').mask(netmask[1]).to_s if version == 4
-        return netmask[1] if version == 6
-      else
-        return '255.255.255.255' if version == 4
-        return 64 if version == 6
-      end
-    end
-    
-    def addresses(rule_value)
-      address_hash = Hash.new()
-	  address_hash['local_subnet'] = []
-      address_hash['ipv4'] = []
-      address_hash['ipv4_range'] = []
-      address_hash['ipv6'] = []
-      address_hash['ipv6_range'] = []
-    
-      rule_value.split(',').each do |address|
-        if address.is_ip?
-          ipaddress = IPAddr.new(address)
-          if ipaddress.ipv4?
-            address_hash['ipv4'].push("#{ipaddress}/#{netmask(address, 4)}")
-          else ipaddress.ipv6?
-            address_hash['ipv6'].push("#{ipaddress}/#{netmask(address, 6)}")
-          end
-        elsif address =~ /^LocalSubnet$/i
-          address_hash['local_subnet'].push('LocalSubnet')
-        else
-          range = Array.new()
-          address.split('-').each do |ip|
-            range.push(IPAddr.new(ip))
-          end
-          if range[0].ipv4?
-            address_hash['ipv4_range'].push(range.join('-'))
-          elsif range[0].ipv6?
-            address_hash['ipv6_range'].push(range.join('-'))
-          end        
-        end
-      end
-    
-      result_array = []
-      address_hash.each do |key, value|
-        if value
-          case key
-          when 'ipv4'
-            result_array += value.sort_by! {|ip| ip.split('/')[0].split('.').map{ |octet| octet.to_i} }
-          when 'ipv6'
-            result_array += value.sort
-          else
-            result_array += value
-          end
-        end
-      end
-    
-      return result_array.join(',')
-    end
-    
-    #If protocol is not TCP or UDP return port as empty string.
-    def ports(protocol, hash_value)
-      if [6, 17].include?(protocol) or protocol == nil
-        return hash_value
-      else
-        return ''
-      end
-    end
     
     #Return equivalent integer value for comma delimited profile names if value is not already an integer.
     def profiles(rule_value, hash)
@@ -134,15 +53,8 @@ Puppet::Type.newtype(:firewall_rule) do
     end
 
     def parse_munge_hash(rule_value, hash, key)
-      case key
-      when 'profiles'
+      if key == 'profiles'
         return profiles(rule_value, hash)
-      when /remote_addresses|local_addresses/i
-        if rule_value !~ /^(\*|LocalSubnet)$/i
-          return addresses(rule_value)
-        else
-          return rule_value
-        end
       else
         if hash[1] != nil
           if hash[1][rule_value]
@@ -158,20 +70,20 @@ Puppet::Type.newtype(:firewall_rule) do
 
     munge_hash = Hash.new()
     munge_hash['ensure'] = [ 'present', nil ]
-    munge_hash['description'] = [ '', nil ]
-    munge_hash['application_name'] = [ '', nil ]
-    munge_hash['service_name'] = [ '', nil ]
+    munge_hash['description'] = [ nil, nil ]
+    munge_hash['application_name'] = [ nil, nil ]
+    munge_hash['service_name'] = [ nil, nil ]
     munge_hash['protocol'] = [ 6, { 'ICMPv4'=>1, 'IGMP'=>2, 'TCP'=>6, 'UDP'=>17, 'IPv6'=>41, 'IPv6Route'=>43, 'IPv6Frag'=>44, 'GRE'=>47, 'ICMPv6'=>58, 'IPv6NoNxt'=>59, 'IPv6Opts'=>60, 'VRRP'=>112, 'PGM'=>113, 'L2TP'=>115 } ]
-    munge_hash['local_ports'] = [ '*', nil ]
-    munge_hash['remote_ports'] = [ '*', nil ]
-    munge_hash['local_addresses'] = [ '*', nil ]
-    munge_hash['remote_addresses'] = [ '*', nil ]
-    munge_hash['icmp_types_and_codes'] = [ '', nil ]
+    munge_hash['local_ports'] = [ nil, nil ]
+    munge_hash['remote_ports'] = [ nil, nil ]
+    munge_hash['local_addresses'] = [ nil, nil ]
+    munge_hash['remote_addresses'] = [ nil, nil ]
+    munge_hash['icmp_types_and_codes'] = [ nil, nil ]
     munge_hash['direction'] = [ 1, { 'In'=>1, 'Out'=>2 } ]
-    munge_hash['interfaces'] = [ nil, nil ]
+    munge_hash['interfaces'] = [ [], nil ]
     munge_hash['interface_types'] = [ 'All', nil ]
     munge_hash['enabled'] = [ true, nil ]
-    munge_hash['grouping'] = [ '', nil]
+    munge_hash['grouping'] = [ nil, nil]
     munge_hash['profiles'] = [ 2147483647, { 'Domain'=>1, 'Private'=>2, 'Public'=>4 } ]
     munge_hash['edge_traversal'] = [ false, nil ]
     munge_hash['action'] = [ 1, { 'Allow'=>1, 'Block'=>0 } ]
@@ -184,16 +96,11 @@ Puppet::Type.newtype(:firewall_rule) do
           if rule.has_key?(key)
             parsed_attr[key] = parse_munge_hash(rule[key], munge_hash[key], key)
           else
-            if ['local_ports', 'remote_ports'].include?(key)
-              parsed_attr[key] = ports(rule['protocol'], munge_hash[key][0] )
-            else
-              parsed_attr[key] = munge_hash[key][0]
-            end
+            parsed_attr[key] = munge_hash[key][0]
           end
         end
         value[name] = parsed_attr
       end
-      value = Digest::MD5.hexdigest Marshal.dump(value.sort.to_s)
     end
   end
 end
