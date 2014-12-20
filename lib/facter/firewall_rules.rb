9710e50804eb02be324da0a3fe46e580cf28af75
Trevor Howard <thoward@godaddy.com>
2014-12-19 17:41:34 -0700

    confine windows-centric stuff in firewall_rules fact

--- lib/facter/firewall_rules.rb
+++ lib/facter/firewall_rules.rb
@@ -1,22 +1,22 @@
-require 'win32ole'
-
-class WIN32OLE
-  def select(attr_name, value)
-    selected = Array.new()
-    self.each do |x|
-      x_value = x.invoke(attr_name)
-      if x_value =~ /^#{value}$/i or x_value == value
-        selected.push(x)
-      end
-    end
-
-    return selected
-  end
-end
-
 Facter.add(:firewall_rules) do
   confine :operatingsystem => 'windows'
   setcode do
+
+    require 'win32ole'
+
+    class WIN32OLE
+      def select(attr_name, value)
+        selected = Array.new()
+        self.each do |x|
+          x_value = x.invoke(attr_name)
+          if x_value =~ /^#{value}$/i or x_value == value
+            selected.push(x)
+          end
+        end
+    
+        return selected
+      end
+    end
 
     system_rules = WIN32OLE.new("HNetCfg.FwPolicy2").rules
     rule_hash = Hash.new()
